f05e8d402c77838a1f34a32978b576cc3ba86bb5
İshak Eren <ishakerenn1905@gmail.com>
2023-12-18 21:52:26 +0300

    completed

--- projects/github_finder/src/components/Navbar.js
+++ projects/github_finder/src/components/Navbar.js
@@ -6,7 +6,7 @@ export class Navbar extends Component {
     return (
       <nav className="navbar navbar-dark bg-primary ">
         <a href="/" className="navbar-brand">
-          <i className={this.props.icon}></i>
+          <i className={this.props.icon} ></i>
           {this.props.title}
         </a>
       </nav>
