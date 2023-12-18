47ea44de897d0a276ac1f9ede435f20515cb6373
İshak Eren <ishakerenn1905@gmail.com>
2023-12-18 15:24:40 +0300

    Github-Finder-Api-Completed

--- projects/github_finder/src/App.js
+++ projects/github_finder/src/App.js
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar";
 import UserList from "./components/UserList";
+import Search from "./components/Search";
+import Alert from "./components/Alert";
 
 import React, { Component } from 'react'
 
@@ -9,24 +11,56 @@ export class App extends Component {
     super(props)
   
     this.state = {
-       users: []
+       loading: false,
+       users: [],
+       error: null
     }
     }
-    componentDidMount(){
-      fetch("https://api.github.com/users")
-        .then(response => response.json())
-        .then(data => this.setState({users: data}))
+    
+    SearchUsers =(keyword) =>
+    {
+      this.setState({ loading: true });
+      setTimeout(() => {
+        fetch("https://api.github.com/search/users?q=" + keyword)
+          .then((response) => response.json())
+          .then((data) => this.setState({ users: data.items, loading: false }));
+      }, 2000);
     }
-  
+
+    ClearUsers = () =>
+    {
+      this.setState(
+        {
+          users: []
+        }
+      )
+    }  
+
+    ErrorControls = (msg, type) =>
+    {
+      this.setState(
+        { error: { msg: msg, type: type } },
+      );
+      setTimeout(() => {
+        this.setState({error: null})
+      }, 3000);
+    }
   render() {
   return (
     <div>
       <Navbar />
+      <Search 
+        SearchUsers={this.SearchUsers} 
+        ClearUsers={this.ClearUsers} 
+        ShowClearButton={this.state.users.length > 0 ? true:false}
+        ErrorControls={this.ErrorControls}
+      />
+      <Alert error={this.state.error}/>
       <div className="container mt-5">
-        <UserList users={this.state.users}/>
+        <UserList users={this.state.users} loading={this.state.loading} />
       </div>
     </div>
-  )
+  );
   }
 }
 
